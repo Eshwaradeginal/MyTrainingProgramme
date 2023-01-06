ceda6efbb9a6abd0ea002e2d3ba0286c824202f7
Eshwaradeginal <102924986+Eshwaradeginal@users.noreply.github.com>
2023-01-06 16:31:15 +0530

    Add files via upload

---
+++ dotNet Programme/NewFolder1/OOPSInheritance.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsCSharp.NewFolder1
+{
+    class Account
+    {
+        public int AccountNo { get; set; }
+        public string AccountName { get; set; }
+        public int Balance { get; protected set; } = 5000;
+    }
+
+    class SBAccount : Account
+    {
+        public void Credit(int amount) => Balance += amount;
+
+        public void Debit(int amount) => Balance -= amount;
+
+    }
+
+    class RDAccount : Account
+    {
+        int amount = 9000;
+        public void MakePayment()
+        {
+            Console.WriteLine($"Payment of Rs. {amount} is done");
+            Balance += amount;
+        }
+    }
+
+
+    class OOPSInheritance
+    {
+        static void Main(string[] args)
+        {
+            SBAccount acc = new SBAccount { AccountName = "testName", AccountNo = 123 };
+            acc.Credit(60000);
+            Console.WriteLine("The Balance : " + acc.Balance);
+            acc.Debit(5000);
+
+            Console.WriteLine("The Balance : " + acc.Balance);
+
+         
+        }
+    }
+}
